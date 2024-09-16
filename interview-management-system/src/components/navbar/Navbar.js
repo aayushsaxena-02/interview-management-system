318e42525cb5c54965e48cdc51e27e66dc2d452d
Shailesh Sawale <shaileshsawale7@gmail.com>
2024-09-16 21:23:43 +0530

    feat: changed layout ui

--- interview-management-system/src/components/navbar/Navbar.js
+++ interview-management-system/src/components/navbar/Navbar.js
@@ -1,13 +1,43 @@
-import styles from './Navbar.module.css'
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./Navbar.module.css";
+import "./../../styles/globals.css";
 
-export default function navbar () {
-    return (
-        <div className = {styles.main}>
-            <h1>navbar</h1>
-            <h1>navbar</h1>
-            <h1>navbar</h1>
-            <h1>navbar</h1>
-            <h1>navbar</h1>
-        </div>
-    )
+export default function Navbar() {
+	const [activeLink, setActiveLink] = useState("/");
+
+	const handleLinkClick = (link) => {
+		setActiveLink(link);
+	};
+
+	const routes = [
+		{ path: "/", label: "Home Page", icon: "" },
+		{ path: "/user", label: "User", icon: "" },
+	];
+
+	return (
+		<div className="flex flex-col h-screen p-6 bg-[#f5f6f6]">
+			<div className="logo mb-6">
+				<h3>Welcome to hiring management system</h3>
+			</div>
+			<nav className="flex flex-col space-y-4">
+				{routes.map((route) => (
+					<Link
+						key={route.path}
+						href={route.path}
+						className={`my-4 py-3 px-6 ${
+							activeLink === route.path
+								? "bg-[#b5aed3] rounded-xl text-[#4f3a63] transition-all"
+								: "border-transparent hover:transition-all"
+						}`}
+						onClick={() => handleLinkClick(route.path)}
+					>
+						{route.label}
+					</Link>
+				))}
+			</nav>
+		</div>
+	);
 }
